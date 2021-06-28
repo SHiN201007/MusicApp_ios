c98633eeb67a669c447f4014541c0725502ce91b
SHiN201007 <shin1007m@gmail.com>
2021-06-28 14:40:37 +0900

    Initial Commit

---
+++ MusicApp_ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicApp_ios
+//
+//  Created by 松丸真 on 2021/06/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
