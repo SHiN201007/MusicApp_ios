cf7770f7a9c99e20612bc0b8802a3d590c48194b
SHiN201007 <shin1007m@gmail.com>
2021-06-28 16:19:05 +0900

    set recommend title UI

--- MusicApp_ios/Views/Home/HomeViewController.swift
+++ MusicApp_ios/Views/Home/HomeViewController.swift
@@ -6,11 +6,20 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class HomeViewController: UIViewController {
-
+  
+  @IBOutlet weak var recommendLabel: UILabel!
+  @IBOutlet weak var recommendDescriptLabel: UILabel!
+  @IBOutlet weak var recommendButton: UIButton!
+  
+  private let disposeBag = DisposeBag()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    
   }
 
 }
@@ -20,6 +29,17 @@ extension HomeViewController {
     layoutConfigure()
   }
   private func layoutConfigure() {
+    // view
     self.view.backgroundColor = .backgroundColor()
+    // recommend
+    /// title
+    recommendLabel.textColor = .mainColor()
+    recommendLabel.font = .boldSystemFont(ofSize: 20)
+    /// descript
+    recommendDescriptLabel.textColor = .textColor()
+    /// button
+    recommendButton.contentHorizontalAlignment = .right
+    recommendButton.setTitleColor(.textColor(), for: .normal)
+    
   }
 }
