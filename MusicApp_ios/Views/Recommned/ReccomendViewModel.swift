88dc858fdadeb9e93d1514c64adbaaf8e11dfdbf
SHiN201007 <shin1007m@gmail.com>
2021-08-24 15:01:18 +0900

    recommendVC UI作成 sampleData表示

--- MusicApp_ios/Views/Recommned/ReccomendViewModel.swift
+++ MusicApp_ios/Views/Recommned/ReccomendViewModel.swift
@@ -8,3 +8,58 @@
 import Foundation
 import RxSwift
 import RxCocoa
+import RxDataSources
+
+struct Recommend {
+  var musicImage: String
+  var title: String
+  var artist: String
+  var preview: Int
+}
+
+struct SectionRecommend {
+  var items: [Item]
+}
+extension SectionRecommend: SectionModelType {
+  typealias Item = Recommend
+
+  init(original: SectionRecommend, items: [SectionRecommend.Item]) {
+    self = original
+    self.items = items
+  }
+}
+
+protocol RecommendViewModelOutput {
+  var items: Observable<[SectionRecommend]> { get }
+}
+protocol RecommendViewModelType {
+  var outputs: RecommendViewModelOutput? { get }
+}
+
+class RecommnedViewModel: RecommendViewModelType {
+  var outputs: RecommendViewModelOutput?
+  
+  private var itemsRelay = BehaviorRelay<[SectionRecommend]>(value: [])
+  
+  init() {
+    self.outputs = self
+    sampleRecommnedData()
+  }
+  
+  private func sampleRecommnedData() {
+    var items: [SectionRecommend] = []
+    
+    for _ in 0...10 {
+      items.append(SectionRecommend(items: [SectionRecommend.Item(musicImage: "oneokrock",
+                                                                  title: "Cry Baby",
+                                                                  artist: "Official髭男dism",
+                                                                  preview: 101232)]))
+    }
+    itemsRelay.accept(items)
+  }
+}
+extension RecommnedViewModel: RecommendViewModelOutput {
+  var items: Observable<[SectionRecommend]> {
+    return itemsRelay.asObservable()
+  }
+}
--- MusicApp_ios/Views/Recommned/RecommendViewController.swift
+++ MusicApp_ios/Views/Recommned/RecommendViewController.swift
@@ -8,14 +8,56 @@
 import UIKit
 import RxSwift
 import RxCocoa
+import RxDataSources
 
 class RecommendViewController: UIViewController {
   
+  @IBOutlet weak var tableView: UITableView!
+  
+  private var viewModel: RecommnedViewModel!
   private let disposeBag = DisposeBag()
+  
+  private lazy var dataSource = RxTableViewSectionedReloadDataSource<SectionRecommend>(configureCell: configureCell)
+  
+  private lazy var configureCell: RxTableViewSectionedReloadDataSource<SectionRecommend>.ConfigureCell = { [weak self]
+    (dataSource, tableView, indexPath, item) in
+    let cell = tableView.dequeueReusableCell(withIdentifier: "RecommendTableViewCell", for: indexPath) as! RecommendTableViewCell
+    
+    cell.recommendMusicImageView.image = UIImage(named: item.musicImage)
+    cell.recommendTitleLabel.text = item.title
+    cell.recommendArtistLabel.text = item.artist
+    cell.recommendPreViewLabel.text = "\(item.preview)回視聴"
+    
+    return cell
+  }
   
   override func viewDidLoad() {
     super.viewDidLoad()
-
+    setupTableView()
+    setupViewModel()
+  }
+  
+  private func setupTableView() {
+    let nib = UINib(nibName: "RecommendTableViewCell", bundle: Bundle.main)
+    tableView.register(nib, forCellReuseIdentifier: "RecommendTableViewCell")
+    tableView.tableFooterView = UIView(frame: .zero) // 空白cellの線　削除
+    tableView.separatorStyle = .none
+    tableView.rx.setDelegate(self).disposed(by: disposeBag)
+    
+    // did selected
+    tableView.rx.itemSelected
+      .subscribe(onNext: { [weak self] indexPath in
+        self?.tableView.deselectRow(at: indexPath, animated: true)
+      })
+      .disposed(by: disposeBag)
+  }
+  
+  private func setupViewModel() {
+    viewModel = RecommnedViewModel()
+    
+    viewModel.outputs?.items
+      .bind(to: tableView.rx.items(dataSource: dataSource))
+      .disposed(by: disposeBag)
   }
 
 }
@@ -27,5 +69,11 @@ extension RecommendViewController {
   
   private func layoutConfigure() {
     self.view.backgroundColor = .backgroundColor()
+    self.tableView.backgroundColor = .backgroundColor()
+  }
+}
+extension RecommendViewController: UITableViewDelegate {
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 70.0
   }
 }
