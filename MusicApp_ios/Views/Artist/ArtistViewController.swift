70fb59583299576a2386c03f9f0efc99676e20a2
SHiN201007 <shin1007m@gmail.com>
2021-08-30 14:37:43 +0900

    artistVC 作成

---
+++ MusicApp_ios/Views/Artist/ArtistViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ArtistViewController.swift
+//  MusicApp_ios
+//
+//  Created by 松丸真 on 2021/08/30.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class ArtistViewController: UIViewController {
+
+  private let disposeBag = DisposeBag()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+  }
+
+}
+extension ArtistViewController {
+  override func viewWillLayoutSubviews() {
+    super.viewWillLayoutSubviews()
+    layoutConfigure()
+  }
+  
+  private func layoutConfigure() {
+    self.view.backgroundColor = .backgroundColor()
+  }
+}
